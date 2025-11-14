382f8974299d89324d0e416c0c7d2812dd8f911d
Kacper <kgrzesik17@gmail.com>
2025-11-14 12:01:36 +0100

    fitness function to evaluate solutions

--- main.py
+++ main.py
@@ -32,3 +32,24 @@ for i, line in enumerate(file):
 # genetic representation of the solution
 def generate_genome(length: int):
   return [1 if random() < 0.1 else 0 for _ in range(length)]
+
+
+# fitness function to evaluate solutions
+def fitness(genome, things, weight_limit: int, item_limit: int):
+  if(len(genome) != len(things)):
+    raise ValueError("Genome and things must be of the same length")
+  
+  weight = 0
+  value = 0
+  item_count = 0
+
+  for i, thing in enumerate(things):
+    if genome[i] == 1:
+      weight += thing.weight
+      value += thing.value
+      item_count += 1
+
+      if weight > weight_limit or item_count > item_limit:
+        return 0
+      
+  return value
